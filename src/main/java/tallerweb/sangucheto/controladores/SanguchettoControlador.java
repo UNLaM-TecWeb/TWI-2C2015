6f93b797ec282f73018990090b3f0b701a9717d2
Manuel <h.torres.baquedano@gmail.com>
2015-11-09 22:44:56 -0300

    Se intenta hacer "Una pantalla que muestre el stock de todos los productos existentes"

---
+++ src/main/java/tallerweb/sangucheto/controladores/SanguchettoControlador.java
@@ -0,0 +1,39 @@
+package tallerweb.sangucheto.controladores;
+
+import java.util.Map;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import tallerweb.sangucheto.modelo.Ingrediente;
+import tallerweb.sangucheto.modelo.Stock;
+import tallerweb.sangucheto.modelo.TipoIngrediente;
+
+
+@Controller
+@RequestMapping("/stock")
+
+
+public class SanguchettoControlador {
+	
+	private Stock s = Stock.getInstance();
+	// Se trata de implementar "Una pantalla que muestre el stock de todos los productos existentes"
+	
+	@RequestMapping(value= "/productos", method=RequestMethod.GET)
+	
+	public ModelAndView stockProdExistentes(){
+		
+		Map<Ingrediente, Integer> stock;
+		stock = s.obtenerStock();
+		
+		ModelAndView mav = new ModelAndView("stock");
+		mav.addObject("verStock", new Ingrediente("Tomate", 0.5, TipoIngrediente.INGREDIENTE));
+		
+		return mav;
+		
+		
+		
+	}
+}
